0e21dc64bc058a07c9ab1512f9d8406353ca98b8
pdJeeves <pdjeeves@zoho.com>
2026-09-06 08:35:54 -0500

    Sun Sep  6 08:35:54 AM CDT 2026

--- include/rintintin.h
+++ include/rintintin.h
@@ -576,6 +576,15 @@ RINTINTIN_API rintintin_vec4 rintintin_compute_rotation_quat(rintintin_eigen con
  */
 RINTINTIN_API int rintintin_compute_inverse_3x3(rintintin_mat3x3 * dst, rintintin_mat3x3 const* src);
 
+typedef enum rintintin_matrix_classification
+{
+	RINTINTIN_POSITIVE		= 1 << 0,
+	RINTINTIN_NEGATIVE		= 1 << 1,
+	RINTINTIN_DEFINITE		= 1 << 2,
+	RINTINTIN_SEMI_DEFINITE	= 1 << 3,
+} rintintin_matrix_classification;
+
+RINTINTIN_API rintintin_matrix_classification rintintin_sym_mat3_classify(rintintin_symmetric_mat3 const* it);
 
 #ifdef __cplusplus
 }
